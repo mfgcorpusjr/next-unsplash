e11723d05b418b97d55c5461f3bb744ff2f36345
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-01-29 19:40:37 +0800

    Fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ImageList from "@/components/ImageList";
 
 export default function HomePage() {
   return (
-    <main className="container mx-auto px-4 py-12">
+    <main className="flex-1 flex flex-col container mx-auto px-4 py-12">
       <ImageList />
     </main>
   );

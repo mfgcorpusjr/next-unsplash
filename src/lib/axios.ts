9c0c8cb9eb3a240912f36152a127f40eb41b0b50
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-01-29 17:24:54 +0800

    Setup axios

---
+++ src/lib/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: `Client-ID ${process.env.ACCESS_KEY}`,
+  },
+});
+
+export default api;
